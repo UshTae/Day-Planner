e2797872cdba44c6ed40e6e631a5692b1d324122
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-04-01 16:10:59 +1100

    adding ids to all buttons and input, and addinf button el to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-//dom elements
+//display todays day using moment
+var today = moment();
+$("#now").text(today.format("LLLL"));
+
+//button elements
+var am9Btn = $('#9am-btn');
+var am10Btn = $('#10am=btn');
+var am11Btn = $('#11am=btn');
+var pm12Btn = $('#12pm=btn');
+var pm1Btn = $('#1pm=btn');
+var pm2Btn = $('#2pm=btn');
+var pm3Btn = $('#3pm=btn');
+var pm4Btn = $('#4pm=btn');
+var pm5Btn = $('#5pm=btn');
 
 //event listeners to all buttons
 
 
-//display todays day using moment
-var today = moment();
-$("#now").text(today.format("LLLL"));
+
+
 
 
 
